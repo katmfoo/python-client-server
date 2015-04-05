3f0a53f092af7068eed159cd7f7ddf30b53bd837
Pat Richeal <patrickricheal@gmail.com>
2015-04-04 21:27:57 -0400

    Tab to spaces

--- client.py
+++ client.py
@@ -5,14 +5,14 @@ import sys
 #Wait for incoming data from server
 #.decode is used to turn the message in bytes to a string
 def receive(socket, signal):
-	while signal:
-		try:
-			data = socket.recv(32)
-			print(str(data.decode("utf-8")))
-		except:
-			print("You have been disconnected from the server")
-			signal = False
-			break
+    while signal:
+        try:
+            data = socket.recv(32)
+            print(str(data.decode("utf-8")))
+        except:
+            print("You have been disconnected from the server")
+            signal = False
+            break
 
 #Get host and port
 host = input("Host: ")
@@ -20,12 +20,12 @@ port = int(input("Port: "))
 
 #Attempt connection to server
 try:
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	sock.connect((host, port))
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((host, port))
 except:
-	print("Could not make a connection to the server")
-	input("Press enter to quit")
-	sys.exit(0)
+    print("Could not make a connection to the server")
+    input("Press enter to quit")
+    sys.exit(0)
 
 #Create new thread to wait for data
 receiveThread = threading.Thread(target = receive, args = (sock, True))
@@ -34,5 +34,5 @@ receiveThread.start()
 #Send data to server
 #str.encode is used to turn the string message into bytes so it can be sent across the network
 while True:
-	message = input()
-	sock.sendall(str.encode(message))
+    message = input()
+    sock.sendall(str.encode(message))
--- server.py
+++ server.py
@@ -9,59 +9,59 @@ total_connections = 0
 #Each instance has the socket and address that is associated with items
 #Along with an assigned ID and a name chosen by the client
 class Client(threading.Thread):
-	def __init__(self, socket, address, id, name, signal):
-		threading.Thread.__init__(self)
-		self.socket = socket
-		self.address = address
-		self.id = id
-		self.name = name
-		self.signal = signal
-	
-	def __str__(self):
-		return str(self.id) + " " + str(self.address)
-	
-	#Attempt to get data from client
-	#If unable to, assume client has disconnected and remove him from server data
-	#If able to and we get data back, print it in the server and send it back to every
-	#client aside from the client that has sent it
-	#.decode is used to convert the byte data into a printable strnig
-	def run(self):
-		while self.signal:
-			try:
-				data = self.socket.recv(32)
-			except:
-				print("Client " + str(self.address) + " has disconnected")
-				self.signal = False
-				connections.remove(self)
-				break
-			if data != "":
-				print("ID " + str(self.id) + ": " + str(data.decode("utf-8")))
-				for client in connections:
-					if client.id != self.id:
-						client.socket.sendall(data)
+    def __init__(self, socket, address, id, name, signal):
+        threading.Thread.__init__(self)
+        self.socket = socket
+        self.address = address
+        self.id = id
+        self.name = name
+        self.signal = signal
+    
+    def __str__(self):
+        return str(self.id) + " " + str(self.address)
+    
+    #Attempt to get data from client
+    #If unable to, assume client has disconnected and remove him from server data
+    #If able to and we get data back, print it in the server and send it back to every
+    #client aside from the client that has sent it
+    #.decode is used to convert the byte data into a printable strnig
+    def run(self):
+        while self.signal:
+            try:
+                data = self.socket.recv(32)
+            except:
+                print("Client " + str(self.address) + " has disconnected")
+                self.signal = False
+                connections.remove(self)
+                break
+            if data != "":
+                print("ID " + str(self.id) + ": " + str(data.decode("utf-8")))
+                for client in connections:
+                    if client.id != self.id:
+                        client.socket.sendall(data)
 
 #Wait for new connections
 def newConnections(socket):
-	while True:
-		sock, address = socket.accept()
-		global total_connections
-		connections.append(Client(sock, address, total_connections, "Name", True))
-		connections[len(connections) - 1].start()
-		print("New connection at ID " + str(connections[len(connections) - 1]))
-		total_connections += 1
+    while True:
+        sock, address = socket.accept()
+        global total_connections
+        connections.append(Client(sock, address, total_connections, "Name", True))
+        connections[len(connections) - 1].start()
+        print("New connection at ID " + str(connections[len(connections) - 1]))
+        total_connections += 1
 
 def main():
-	#Get host and port
-	host = input("Host: ")
-	port = int(input("Port: "))
+    #Get host and port
+    host = input("Host: ")
+    port = int(input("Port: "))
 
-	#Create new server socket
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	sock.bind((host, port))
-	sock.listen(5)
+    #Create new server socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((host, port))
+    sock.listen(5)
 
-	#Create new thread to wait for connections
-	newConnectionsThread = threading.Thread(target = newConnections, args = (sock,))
-	newConnectionsThread.start()
-	
+    #Create new thread to wait for connections
+    newConnectionsThread = threading.Thread(target = newConnections, args = (sock,))
+    newConnectionsThread.start()
+    
 main()
